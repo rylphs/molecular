2027bed71c8cb53ca4027c45b7a191103d08ab27
Raphael <rylphs@gmail.com>
2017-07-21 19:40:04 -0300

    Adicao de script para geracao rapida da build no molecular-starter.

---
+++ tools/build-molecular.sh
@@ -0,0 +1,20 @@
+(
+	molecular=~/workspaces/molecular;
+	starter=~/workspaces/molecular-starter;
+
+	appmodule=~/workspaces/molecular-starter/src/app/app.module.ts;
+	dst=~/workspaces/molecular-starter/node_modules/molecular;
+
+	cd $molecular;
+	rm -rf ./build/*
+	npm run build && { 
+		rm -rf $dst/*
+		ls | grep -v 'node_modules' |xargs -I xx cp -avr xx $dst/;
+		touch $dst/build/renderer.js;
+		touch $dst/build/main.js;
+		touch $starter/dist/molecular.bundle.js;
+		touch $appmodule;
+	};
+
+)
+
